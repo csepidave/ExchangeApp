4b5fa09deb0e7d99207c778fa564a8a2dee3dc23
David <csepidave@gmail.com>
2018-10-11 22:34:28 +0200

    Add views
    Create fragments and layouts for bottom navigation bar

--- app/src/main/java/exchangerates/com/csepi/exchangerates/MainActivity.kt
+++ app/src/main/java/exchangerates/com/csepi/exchangerates/MainActivity.kt
@@ -2,29 +2,48 @@ package exchangerates.com.csepi.exchangerates
 
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.Fragment
+import android.support.v7.app.ActionBar
 import android.support.v7.app.AppCompatActivity
+import exchangerates.com.csepi.exchangerates.presentation.view.ActualRatesFragment
+import exchangerates.com.csepi.exchangerates.presentation.view.HistoricalRatesFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+    lateinit var toolbar: ActionBar
+    private val actualRatesFragment: ActualRatesFragment by lazy { ActualRatesFragment.newInstance() }
+    private val historicalRatesFragment: HistoricalRatesFragment by lazy { HistoricalRatesFragment.newInstance() }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        toolbar = supportActionBar!!
+        navigation.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+    }
+
+    private val onNavigationItemSelectedListener =
+            BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
-            R.id.navigation_home -> {
-                message.setText(R.string.title_latest)
+            R.id.navigation_actual -> {
+                toolbar.title = applicationContext.resources.getString(R.string.title_actual)
+                openFragment(actualRatesFragment)
                 return@OnNavigationItemSelectedListener true
             }
-            R.id.navigation_dashboard -> {
-                message.setText(R.string.title_historical)
+            R.id.navigation_historical -> {
+                toolbar.title = applicationContext.resources.getString(R.string.title_historical)
+                openFragment(historicalRatesFragment)
                 return@OnNavigationItemSelectedListener true
             }
         }
         false
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    private fun openFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
     }
 }

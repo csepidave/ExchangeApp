0ffbbe0e3feb3435b7c64b91c069780d409b9168
David <csepidave@gmail.com>
2018-10-11 19:44:11 +0200

    Fix MainActivity

--- app/src/main/java/exchangerates/com/csepi/exchangerates/MainActivity.kt
+++ app/src/main/java/exchangerates/com/csepi/exchangerates/MainActivity.kt
@@ -10,15 +10,11 @@ class MainActivity : AppCompatActivity() {
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_home -> {
-                message.setText(R.string.title_home)
+                message.setText(R.string.title_latest)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_dashboard -> {
-                message.setText(R.string.title_dashboard)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_notifications -> {
-                message.setText(R.string.title_notifications)
+                message.setText(R.string.title_historical)
                 return@OnNavigationItemSelectedListener true
             }
         }
